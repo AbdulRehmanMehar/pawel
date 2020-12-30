eaecb32156fd1b81df8b39ab359b943f030a130c
Abdul Rehman <mehars.6925@gmail.com>
2020-12-30 22:42:35 +0500

    created home component and added route for login

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h3>Welcome Pawel!</h3>
+        <p>Here are the links to screens.</p>
+
+        <ul>
+            <li>
+                <router-link :to="{name: 'login'}">Login</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
