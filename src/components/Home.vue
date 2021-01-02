7311c7579fbdb8bc720a43d03725ddc02b0129b5
Abdul Rehman <mehars.6925@gmail.com>
2021-01-03 02:43:05 +0500

    added the link for employee-create-ticket route

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,6 +7,12 @@
             <li>
                 <router-link :to="{name: 'login'}">Login</router-link>
             </li>
+            <li>
+                <b>Employee Routes</b>
+            </li>
+            <li>
+                <router-link :to="{name: 'employee-create-ticket'}">Create Ticket</router-link>
+            </li>
         </ul>
     </div>
 </template>
